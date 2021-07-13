b4c7b92d2ec33ebd1c5886e63573b105bdffb240
Emanuel-Ramos <manuel_live_ac@hotmail.com>
2021-07-13 17:05:49 -0300

    .

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,20 +1,54 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import Home from '../screens/Home';
 import Signin from '../screens/Signin'
 import Products from '../screens/Products'
 import Login from '../screens/Login'
+import Icon from '@expo/vector-icons/AntDesign';
 
-const AuthStack = createBottomTabNavigator()
+const AuthStack = createStackNavigator()
+const AuthBottom = createBottomTabNavigator();
+
+const icons = {
+    Home: {
+        name: 'home'
+    },
+    Signin: {
+        name: 'adduser'
+    },
+    Login: {
+        name: 'login'
+    },
+    Products: {
+        name: 'tags'
+    },
+    Cart: {
+        name: 'shoppingcart'
+    },
+}
 
 function AuthRoutes() {
     return (
-        <AuthStack.Navigator>
-            <AuthStack.Screen name="Home" component={Home} />
-            <AuthStack.Screen name="Signin" component={Signin} />
-            <AuthStack.Screen name="Login" component={Login} />
-            <AuthStack.Screen name="Products" component={Products} />
-        </AuthStack.Navigator>
+        <AuthBottom.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ color, size }) => {
+                    const { name } = icons[route.name];
+                    return <Icon name={name} color={color} size={size} />
+                }
+            })}
+            tabBarOptions={{
+                style: {
+                    backgroundColor: '#121212'
+                },
+                activeTintColor: '#FFF',
+            }}
+        >
+            <AuthBottom.Screen name="Home" component={Home} />
+            <AuthBottom.Screen name="Signin" component={Signin} />
+            <AuthBottom.Screen name="Login" component={Login} />
+            <AuthBottom.Screen name="Products" component={Products} />
+        </AuthBottom.Navigator>
     );
 }
 
