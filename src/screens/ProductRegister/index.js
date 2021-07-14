1c487ec57cce9d471e58950a9c8efa9c16bc9f19
GuilhermeSantosmain <guilhermesantosmain@gmail.com>
2021-07-14 16:32:10 -0300

    product registerr

--- src/screens/ProductRegister/index.js
+++ src/screens/ProductRegister/index.js
@@ -8,9 +8,39 @@ function ProductRegister() {
     const navigation = useNavigation();
     const { http } = useContext(AuthContext)
 
-    const [produtc, setNewProduct] = useState('')
+    const [codigo, setCodigo] = useState('')
+    const [descricaoProduto, setDescricaoProduto] = useState('')
+    const [quantidadeEstoque, setQuantidadeEstoque] = useState('')
+    const [nomeProduto, setNomeProduto] = useState('')
+    const [preco, setPreco] = useState('')
+    const [categoria, setCategoria] = useState('')
+    
+    const [nomeCategoria, setNomeCategoria] = useState('')
+    const [descricaoCategoria, setDescricaoCategoria] = useState('')
+
+    const [newProduct, setNewProduct] = useState({})
+    const [newCategory, setNewCategory] = useState({})
 
     async function handleNewProduct() {
+        const produto = {
+            codigo: codigo,
+            nome: nomeProduto, 
+            descricao: descricaoProduto,
+            preco: preco,
+            quantidadeEstoque: quantidadeEstoque,
+            categoria: categoria
+        }
+        console.log(produto)
+        setNewProduct(produto)
+        http.post('produto', newProduct).then(console.log("Produto cadastrado")).catch(erro => console.log(erro))
+    }
+    async function handleNewCategory() {
+        const categoria = {
+            nome: nomeCategoria,
+            descricao: descricaoCategoria
+        }
+        setNewCategory(categoria)
+        http.post('categoria', newCategory).then(console.log("categoria cadastrada")).catch(erro => console.log(erro))
 
     }
 
@@ -18,12 +48,65 @@ function ProductRegister() {
         <View style={styles.inputs}>
 
             <View style={styles.txts}>
+
+            <View style={styles.inputTxt} >
+                    <View style={styles.inputTxtRow}>
+                        <Text>Codigo do Produto</Text>
+                        <TextInput style={styles.input} onChangeText={(e) => setNomeCategoria(e) } value={nomeCategoria} />
+                    </View>
+                        
+                    <View style={styles.inputTxtRow}>
+                        <Text>Nome</Text>
+                        <TextInput style={styles.input} onChangeText={(e) => setDescricaoCategoria(e) } value={descricaoCategoria} />
+                    </View>
+                    
+                                                    
+
+                </View>
+                <View style={styles.msgCadastro}>
+                    <TouchableOpacity onPress={handleNewCategory}>
+                        <Text style={styles.msgBtn}>Cadastrar Categoria</Text>
+
+                    </TouchableOpacity>
+                </View>
+
+
                 <View style={styles.inputTxt} >
-                    <TextInput style={styles.input} onChange={(e) => setNewProduct(e.target.value)} />
+                    <View style={styles.inputTxtRow}>
+                        <Text>Codigo do Produto</Text>
+                        <TextInput style={styles.input} onChangeText={(e) => setCodigo(e) } value={codigo}/>
+                    </View>
+                        
+                    <View style={styles.inputTxtRow}>
+                        <Text>Nome</Text>
+                        <TextInput style={styles.input} onChangeText={(e) => setNomeProduto(e) } value={nomeProduto}/>
+                    </View>
+                    
+                    <View style={styles.inputTxtRow}>
+                        <Text>Descrição</Text>
+                        <TextInput style={styles.input} onChangeText={(e) => setDescricaoProduto(e) } value={descricaoProduto}/>
+                    </View>
+                    
+                    <View style={styles.inputTxtRow}>
+                        <Text>Preço</Text>
+                        <TextInput style={styles.input} onChangeText={(e) => setPreco(e) } value={preco}/>
+                    </View>
+                    
+                    <View style={styles.inputTxtRow}>
+                        <Text>Quantidade de estoque</Text>
+                        <TextInput style={styles.input} onChangeText={(e) => setQuantidadeEstoque(e) } value={quantidadeEstoque}/>
+                    </View>
+                    
+                    <View style={styles.inputTxtRow}>
+                        <Text>Categoria</Text>
+                        <TextInput style={styles.input} onChangeText={(e) => setCategoria(e) } value={categoria}/>
+                    </View>              
+                    
+
                 </View>
                 <View style={styles.msgCadastro}>
                     <TouchableOpacity onPress={handleNewProduct}>
-                        <Text style={styles.msgBtn}>Cadastrar</Text>
+                        <Text style={styles.msgBtn}>Cadastrar Produto</Text>
 
                     </TouchableOpacity>
                 </View>
@@ -42,7 +125,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
     },
-
+    inputTxtRow:{
+        flexDirection:"row"
+    },
     inputTxt: {
         borderColor: '#000',
         borderWidth: 2,
