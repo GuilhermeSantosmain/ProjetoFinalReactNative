ff289501c04049cdf05f815552cf42f394014cac
Gustavo Xavier <gustavosaldxav@gmail.com>
2021-07-15 18:02:08 -0300

    Senha Input atualizado

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -2,6 +2,7 @@ import React, { useState, useContext } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { View, Text, StyleSheet, TextInput, TouchableOpacity, Dimensions } from 'react-native';
 import { AuthContext } from '../../contexts/auth'
+import { Entypo } from '@expo/vector-icons'
 import firebase from 'firebase';
 import 'firebase/database'
 
@@ -13,6 +14,8 @@ function Login() {
     const { logIn } = useContext(AuthContext)
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [hidePass, setHidePass] = useState(true)
+  
 
     function handleLogin() {
         logIn(email, password)
@@ -27,8 +30,17 @@ function Login() {
                     <View style={styles.inputTxt} >
                         <TextInput value={email} placeholder="E-mail" style={styles.input} onChangeText={(e) => setEmail(e)} />
                     </View>
-                    <View style={styles.inputTxt}>
-                        <TextInput value={password} placeholder="Senha" style={styles.input} onChangeText={(e) => setPassword(e)} />
+                    <View style={styles.inputSenha}>
+                        <TextInput secureTextEntry={hidePass} value={password} placeholder="Senha" style={styles.input} onChangeText={(e) => setPassword(e)} />
+                        <TouchableOpacity style={styles.iconSenha} onPress={ () => setHidePass(!hidePass)}>
+                            { hidePass ?
+                            <Entypo name="eye" color="#000" size={25} /> 
+                            :
+                            <Entypo name="eye-with-line" color="#000" size={25} /> 
+                            }
+                        </TouchableOpacity>
+                    
+                    
                     </View>
                 </View>
 
@@ -76,8 +88,25 @@ const styles = StyleSheet.create({
         marginBottom: windowHeight / 30,
     },
 
+    inputSenha:{
+
+        borderColor: '#000',
+        borderWidth: 0.5,
+        borderRadius: 10,
+        width: windowWidth / 1.5,
+        padding: 5,
+        marginBottom: windowHeight / 30,
+        flexDirection: 'row'
+    },
+
+    iconSenha:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '20%',
+    },
+
     input: {
-        width: '100%',
+        width: '90%',
         height: windowHeight / 15,
         padding: 3,
         textDecorationLine: 'none'
