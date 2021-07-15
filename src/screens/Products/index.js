f0d915153a38c7d128d6365698abe2868479990e
GabrielGalhardo <galhardogabriel843@gmail.com>
2021-07-14 21:48:51 -0300

    produtos - parcial

--- src/screens/Products/index.js
+++ src/screens/Products/index.js
@@ -1,34 +1,106 @@
-import React, {useContext} from 'react';
-import { View, Text } from 'react-native'
-import { AuthContext } from '../../contexts/auth'
+import React, { useContext, useEffect, useState } from "react";
+import {
+  View,
+  FlatList,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  SafeAreaView,
+} from "react-native";
+import { AuthContext } from "../../contexts/auth";
 
-const Products = (props) => {
-    
-    const {produtos} = useContext(AuthContext);
-    
-if (produtos.lenght === 0){
+const itens = [
+  {
+    id: "ABC",
+    url: "https://images.lojanike.com.br/500x500/produto/tenis-nike-sb-nyjah-free-2-unissex-BV2078-103-1.jpg",
+    nome: "Tênis Nike SB Nyjah Free 2 Unissex",
+    preco: "R$ 699,99",
+  },
+  {
+    id: "ABCD",
+    url: "https://images.lojanike.com.br/500x500/produto/tenis-nike-air-max-97-masculino-DB4611-002-1.jpg",
+    nome: "Tênis Nike Air Max 97 Masculino",
+    preco: "R$ 519,99",
+  },
+  {
+    id: "ABCDE",
+    url: "https://images.lojanike.com.br/500x500/produto/tenis-nike-air-zoom-gt-cut-unissex-CZ0175-101-1.jpg",
+    nome: "Tênis Nike Air Zoom G.T. Cut Unissex",
+    preco: "R$ 1.099,99",
+  },
+  {
+    id: "ABCDEF",
+    url: "https://images.lojanike.com.br/500x500/produto/tenis-kyrie-7-seasonal-CZ0141-100-1.jpg",
+    nome: "Tênis Nike Kyrie 7 Masculino",
+    preco: "R$ 749,99",
+  },
+  {
+    id: "ABCDEFG",
+    url: "https://images.lojanike.com.br/500x500/produto/tenis-nike-air-max-zm950-masculino-CJ6700-001-1.jpg",
+    nome: "Tênis Nike Air Max ZM950 Masculino",
+    preco: "R$ 619,99 ",
+  },
+  {
+    id: "ABCDEFGH",
+    url: "https://images.lojanike.com.br/500x500/produto/tenis-nike-air-max-zm950-masculino-CJ6700-001-1.jpg",
+    nome: "Tênis Nike Air Max ZM950 Masculino",
+    preco: "R$ 619,99 ",
+  },
+];
 
-    return (
-        <View>
-            <Text>
-                Nao ha nada aqui
-            </Text>
-        </View>
-    )
-    }else {
+const renderItem = ({ item }) => (
+  <TouchableOpacity>
+    <View style={styles.card}>
+      <Image source={{ uri: item.url }} style={styles.img} />
+      <Text style={styles.title}>{item.nome}</Text>
+      <Text style={styles.title}>{item.preco}</Text>
+    </View>
+  </TouchableOpacity>
+);
 
-        return (
-            <View>
-                <Text>
+const Products = () => {
+  const { produtos } = useContext(AuthContext);
 
-                {produtos.map((item) =>
-                            {item.nome}
-                )}
-                </Text>
-            </View>
-        )
-    }
-    
-}
+  return (
+    <SafeAreaView>
+      <FlatList
+        data={itens}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        pagingEnabled
+        numColumns={2}
+      />
+    </SafeAreaView>
+  );
+};
 
-export default Products
+export default Products;
+
+const styles = StyleSheet.create({
+  img: {
+    width: "100%",
+    height: 70,
+  },
+
+  card: {
+    width: 150,
+    height: 200,
+    borderWidth: 1,
+    margin: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  container: {
+    flex: 1,
+    display: "flex",
+    justifyContent: "space-around",
+    backgroundColor: "red",
+    flexDirection: "row",
+  },
+
+  title: {
+    marginTop: 10,
+  },
+});
