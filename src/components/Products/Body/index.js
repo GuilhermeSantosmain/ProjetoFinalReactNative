50681cfead8ae55183d3ea52636bc566700d4231
Emanuel-Ramos <manuel_live_ac@hotmail.com>
2021-07-15 21:54:39 -0300

    .

--- src/components/Products/Body/index.js
+++ src/components/Products/Body/index.js
@@ -9,44 +9,6 @@ import { AuthContext } from "../../../contexts/auth";
 import ProductCard from '../Card/index';
 import { useNavigation } from "@react-navigation/native";
 
-const itens = [
-  {
-    id: "ABC",
-    url: "https://images.lojanike.com.br/500x500/produto/tenis-nike-sb-nyjah-free-2-unissex-BV2078-103-1.jpg",
-    nome: "Tênis Nike SB Nyjah Free 2 Unissex",
-    preco: "R$ 699,99",
-  },
-  {
-    id: "ABCD",
-    url: "https://images.lojanike.com.br/500x500/produto/tenis-nike-air-max-97-masculino-DB4611-002-1.jpg",
-    nome: "Tênis Nike Air Max 97 Masculino",
-    preco: "R$ 519,99",
-  },
-  {
-    id: "ABCDE",
-    url: "https://images.lojanike.com.br/500x500/produto/tenis-nike-air-zoom-gt-cut-unissex-CZ0175-101-1.jpg",
-    nome: "Tênis Nike Air Zoom G.T. Cut Unissex",
-    preco: "R$ 1.099,99",
-  },
-  {
-    id: "ABCDEF",
-    url: "https://images.lojanike.com.br/500x500/produto/tenis-kyrie-7-seasonal-CZ0141-100-1.jpg",
-    nome: "Tênis Nike Kyrie 7 Masculino",
-    preco: "R$ 749,99",
-  },
-  {
-    id: "ABCDEFG",
-    url: "https://images.lojanike.com.br/500x500/produto/tenis-nike-air-max-zm950-masculino-CJ6700-001-1.jpg",
-    nome: "Tênis Nike Air Max ZM950 Masculino",
-    preco: "R$ 619,99 ",
-  },
-  {
-    id: "ABCDEFGH",
-    url: "https://images.lojanike.com.br/500x500/produto/tenis-nike-air-max-zm950-masculino-CJ6700-001-1.jpg",
-    nome: "Tênis Nike Air Max ZM950 Masculino",
-    preco: "R$ 619,99 ",
-  },
-];
 
 const Products = () => {
   const [produtos, setProdutos] = useState([]);
