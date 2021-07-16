133d32d998c7725f352bab3c794bd017f2f24185
GuilhermeSantosmain <guilhermesantosmain@gmail.com>
2021-07-16 13:57:44 -0300

    ProductEdit voltando

--- src/screens/ProductEdit/index.js
+++ src/screens/ProductEdit/index.js
@@ -13,7 +13,8 @@ function ProductEdit() {
 
     const [selectedValue, setSelectedValue] = useState("Selecione um produto");
     const [selectedValueCategoria, setSelectedValueCategoria] = useState("");
-
+    
+ 
     const [codigo, setCodigo] = useState('')
     const [descricaoProduto, setDescricaoProduto] = useState('')
     const [quantidadeEstoque, setQuantidadeEstoque] = useState('')
@@ -21,7 +22,7 @@ function ProductEdit() {
     const [preco, setPreco] = useState('')
     const [categoria, setCategoria] = useState('')
 
-    
+
     const [produtos, setProdutos] = useState([])
     const [categorias, setCategorias] = useState([])
 
@@ -36,102 +37,118 @@ function ProductEdit() {
             categoria: categoria
         }
         console.log(selectedValue.id)
-        http.put(`produto/${selectedValue.id}` , produto).then(console.log("Produto cadastrado")).catch(erro => console.log(erro))
+        http.put(`produto/${selectedValue.id}`, produto).then(console.log("Produto cadastrado")).catch(erro => console.log(erro))
+        navigation.goBack()
     }
 
-     function handleProductEdit(produtoEdit) {
+    function handleProductEdit(produtoEdit) {
         console.log("foi");
         setSelectedValue(produtoEdit)
-            setCategoria(produtoEdit.categoria)
-            setCodigo(produtoEdit.codigo)
-            setDescricaoProduto(produtoEdit.descricao)
-            setQuantidadeEstoque((produtoEdit.quantidadeEstoque).toString())
-            setNomeProduto(produtoEdit.nome)
-            setPreco((produtoEdit.preco).toString())
+        setSelectedValueCategoria(produtoEdit.categoria)
+        setQuantidadeEstoque((produtoEdit.quantidadeEstoque).toString())
+        setCategoria(produtoEdit.categoria)
+        setCodigo(produtoEdit.codigo)
+        setDescricaoProduto(produtoEdit.descricao)
+        setNomeProduto(produtoEdit.nome)
+        setPreco((produtoEdit.preco).toString())
         
+    }
+    function handleProductDelete(produtoDelete) {
+        http.delete(`produto/${produtoDelete.id}`).then(console.log(`o produto ${produtoDelete.nome} foi deletado com sucesso`)).catch(erro => console.log(erro))
+        navigation.goBack()
 
     }
     useEffect(() => {
-        http.get('produto/todos').then((response) => { setProdutos(response.data) })
-        http.get('categoria/todas').then((response) => {setCategorias(response.data)})
-    },[])
+        http.get('produto/todos').then((response) => { setProdutos(response.data) }).catch(erro => console.log(erro))
+        http.get('categoria/todas').then((response) => { setCategorias(response.data) }).catch(erro => console.log(erro))
+
+    }, [])
 
 
     return (
-        <TouchableWithoutFeedback onPress={ () => Keyboard.dismiss()}>
-        <View style={styles.body}>
-            <View style={styles.form}>
-                <View style={styles.inputs}>
-                    <View style={styles.inputView}>
-                        <Text style={styles.inputText}>Produto</Text>
-                        <Picker
-                            style={styles.picker}
-                            selectedValue={selectedValue}
-                            onValueChange={(itemValue) => {                             
-                            handleProductEdit(itemValue)
-                            }}>
-                            {produtos.map((item) => {
-                               
-                                return <Picker.Item key={item.id} label={item.nome} value={item} />
-                            })}
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <View style={styles.body}>
+                <View style={styles.form}>
+                    <View style={styles.inputs}>
+                        <View style={styles.inputView}>
+                            <Text style={styles.inputText}>Produto</Text>
+                            <Picker
+                                style={styles.picker}
+                                selectedValue={selectedValue}
+                                onValueChange={(itemValue) => {
+                                    handleProductEdit(itemValue)
+                                }}>
+                                <Picker.Item label="Escolha um produto" value="0" />
+                                {produtos.map((item) => {
 
-                        </Picker>
+                                    return <Picker.Item key={item.id} label={item.nome} value={item} />
+                                })}
+
+                            </Picker>
+                        </View>
+
+                        <View style={styles.inputView}>
+                            <Text style={styles.inputText}>Codigo do Produto</Text>
+                            <TextInput style={styles.input} onChangeText={(e) => setCodigo({nome: e})} defaultValue={codigo} />
+                        </View>
+
+                        <View style={styles.inputView}>
+                            <Text style={styles.inputText}>Nome</Text>
+                            <TextInput style={styles.input} onChangeText={(e) => setNomeProduto(e)} defaultValue={nomeProduto} />
+                        </View>
+
+                        <View style={styles.inputView}>
+                            <Text style={styles.inputText}>Descrição</Text>
+                            <TextInput style={styles.input} onChangeText={(e) => setDescricaoProduto(e)} defaultValue={descricaoProduto} />
+                        </View>
+
+                        <View style={styles.inputView}>
+                            <Text style={styles.inputText}>Preço</Text>
+                            <TextInput style={styles.input} onChangeText={(e) => setPreco(e)} defaultValue={preco} />
+                        </View>
+
+                        <View style={styles.inputView}>
+                            <Text style={styles.inputText}>Quantidade de estoque</Text>
+                            <TextInput style={styles.input} onChangeText={(e) => setQuantidadeEstoque(e)} defaultValue={quantidadeEstoque} />
+                        </View>
+
+                        <View style={[styles.inputView, { marginBottom: 15 }]}>
+                            <Text style={styles.inputText}>Categoria</Text>
+                            <Picker
+                                style={styles.picker}
+                                selectedValue={selectedValueCategoria}
+                                onValueChange={(itemValue) => {
+                                    setSelectedValueCategoria(itemValue)
+                                }}>
+                                <Picker.Item label="Escolha uma categoria" value="0" />
+                                {categorias.map((item) => {
+
+                                    return <Picker.Item key={item.id} label={item.nome} value={item} />
+                                })}
+
+                            </Picker>
+                        </View>
+
                     </View>
 
-                    <View style={styles.inputView}>
-                        <Text style={styles.inputText}>Codigo do Produto</Text>
-                        <TextInput style={styles.input} onChangeText={(e) => setCodigo(e)} defaultValue={codigo} />
+
+
+                    <View style={[styles.msgCadastro, styles.inputView]}>
+                        <TouchableOpacity style={styles.btn} onPress={() => handleProduct()}>
+                            <Text style={styles.tituloBtn}>Editar Produto</Text>
+
+                        </TouchableOpacity>
                     </View>
 
-                    <View style={styles.inputView}>
-                        <Text style={styles.inputText}>Nome</Text>
-                        <TextInput style={styles.input} onChangeText={(e) => setNomeProduto(e)} defaultValue={nomeProduto} />
+                    <View style={[styles.msgCadastro, styles.inputView, styles.btnDelete]}>
+                        <TouchableOpacity onPress={() => handleProductDelete(selectedValue)}>
+                            <Text style={styles.tituloBtn}>Deletar Produto</Text>
+
+                        </TouchableOpacity>
                     </View>
 
-                    <View style={styles.inputView}>
-                        <Text style={styles.inputText}>Descrição</Text>
-                        <TextInput style={styles.input} onChangeText={(e) => setDescricaoProduto(e)} defaultValue={descricaoProduto} />
-                    </View>
-
-                    <View style={styles.inputView}>
-                        <Text style={styles.inputText}>Preço</Text>
-                        <TextInput style={styles.input} onChangeText={(e) => setPreco(e)} defaultValue={preco} />
-                    </View>
-
-                    <View style={styles.inputView}>
-                        <Text style={styles.inputText}>Quantidade de estoque</Text>
-                        <TextInput style={styles.input} onChangeText={(e) => setQuantidadeEstoque(e)} defaultValue={quantidadeEstoque} />
-                    </View>
-
-                    <View style={styles.inputView}>
-                        <Text style={styles.inputText}>Categoria</Text>
-                        <Picker
-                            style={styles.picker}
-                            selectedValue={selectedValueCategoria}
-                            onValueChange={(itemValue) => {
-                            setSelectedValueCategoria(itemValue) 
-                            }}>
-                            {categorias.map((item) => {
-                               
-                                return <Picker.Item key={item.id} label={item.nome} value={item} />
-                            })}
-
-                        </Picker>
-                    </View>
-
-                </View>
-
-
-
-                <View style={[styles.msgCadastro, styles.inputView]}>
-                    <TouchableOpacity style={styles.btn} onPress={handleProduct}>
-                        <Text style={styles.tituloBtn}>Editar Produto</Text>
-
-                    </TouchableOpacity>
-                </View>
-
-            </View >
-        </View>
+                </View >
+            </View>
 
         </TouchableWithoutFeedback>
 
